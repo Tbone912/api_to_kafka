b242a843748c876a8d2d4cbf619b031669215da5
Tbone912 <56204801+Tbone912@users.noreply.github.com>
2023-04-22 21:59:45 -0400

    Makes POST call to API2

--- src/main/java/com/kafka/controller/KafkaProducerController.java
+++ src/main/java/com/kafka/controller/KafkaProducerController.java
@@ -1,7 +1,5 @@
 package com.kafka.controller;
 
-
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,24 +16,25 @@ import com.kafka.service.KafKaProducerService;
 
 @RestController
 @RequestMapping(value = "/kafka")
-public class KafkaProducerController
-{
+public class KafkaProducerController {
 	private final KafKaProducerService producerService;
 
 	@Autowired
-	public KafkaProducerController(KafKaProducerService producerService)
-	{
+	public KafkaProducerController(KafKaProducerService producerService) {
 		this.producerService = producerService;
 	}
 
 	@PostMapping(value = "/publish")
-	public void sendMessageToKafkaTopic(@RequestBody List<User> users) throws JsonProcessingException
-	{
-		
-		
+	public void sendMessageToKafkaTopic(@RequestBody List<User> users) throws JsonProcessingException {
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		String user = objectMapper.writeValueAsString(users);
-//		System.err.println(user);
 		this.producerService.sendMessage(user);
+
+		RestTemplate restTemplate = new RestTemplate();
+		final String uri = "http://localhost:9001/s3/upload";
+		String reqBody = "ABCDEF";
+
+		String result = restTemplate.postForObject(uri, reqBody, String.class);
 	}
 }
